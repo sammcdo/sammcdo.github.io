589ff957c2c2cfe49608387b0518903ee3724eac
sammcdo <sam_mcdo@yahoo.com>
2023-11-09 16:21:07 -0500

    more resume changes

--- js/index.js
+++ js/index.js
@@ -3,9 +3,14 @@
 $("p.skills").hover(
     (e) => {
         console.log("here");
-        $(e.currentTarget).css("font-size", "18px");
+        $(e.currentTarget)
+        .css("font-size", "18px")
+        .css("padding-bottom", "0px")
+        .css("padding-top", "0px");
     }, (e) => {
-        $(e.currentTarget).css("font-size", "16px");
+        $(e.currentTarget).css("font-size", "16px")
+        .css("padding-bottom", "1px")
+        .css("padding-top", "1px");
     }
 );
 
